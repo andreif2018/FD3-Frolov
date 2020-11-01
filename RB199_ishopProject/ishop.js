db5d5ab8a1cde85321a9dd837686240a75fbdde0
Andrei <andreifrolov83@gmail.com>
2020-11-01 10:08:46 +0300

    adding forEach

--- RB199_ishopProject/ishop.js
+++ RB199_ishopProject/ishop.js
@@ -14,17 +14,17 @@ var ShopBlock = React.createClass({
     render: function() {
 
         var itemsCode = [];
-        for ( var a = 0; a < this.props.items.length; a++ ) {
-            var item = this.props.items[a];
-            var itemCode=
+        this.props.items.forEach( (item) => {
+            var itemCode =
                 React.DOM.tr({ key:item.code, className:'ItemCode' },
                     React.DOM.td({className:'Text'}, item.item),
                     React.DOM.td({className:'Price'}, item.price + " $"),
                     React.DOM.td({className:'Count'}, item.count + " шт"),
                     React.DOM.td({className:'URLPhoto'}, React.DOM.img({src: item.urlPhoto}, ),
-                ));
+                    ));
             itemsCode.push(itemCode);
-        }
+        })
+
         return React.DOM.table( {className:'ShopBlock'},
             React.DOM.thead( {}, React.DOM.tr( {}, React.DOM.th({className:'Title', colSpan: "4"}, this.props.title ))),
 
