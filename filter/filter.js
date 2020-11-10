dd6fbdf3dbfbb1954bacb8fd1cfa3d418f8d2ba4
Andrei <andreifrolov83@gmail.com>
2020-11-10 22:04:42 +0300

    update task filter

--- filter/filter.js
+++ filter/filter.js
@@ -18,6 +18,10 @@ var FilterBlock = React.createClass({
         };
     },
 
+    textChanged: function(EO) {
+        console.log('текст ввода изменён - ' + EO.target.value);
+    },
+
     render: function () {
 
         var itemsCode = [];
@@ -29,12 +33,15 @@ var FilterBlock = React.createClass({
             } );
             itemsCode.push(element);
         });
-        return React.DOM.div({className: "ShopBlock"}, React.DOM.div({className: "Title"}, this.props.title),
-                React.DOM.input({type:"checkbox"},),
-                React.DOM.input({type:"text"},),
-                React.DOM.button({},"сброс"),
+
+        return React.DOM.div( {className: "ShopBlock"}, React.DOM.div({className: "Title"}, this.props.title),
+                React.DOM.form( {},
+                    React.DOM.input({type: "checkbox", defaultChecked: false}, ),
+                    React.DOM.input({type: "text", onChange: this.textChanged}, ),
+                    React.DOM.input({type: "reset", defaultValue: "сброс"}, ),
+                    ),
                 React.DOM.br({},),
-                React.DOM.select({className: "DropDown", defaultValue: ""}, React.DOM.option( {className: "Item",  disabled: true}, ), itemsCode ), // first option tag is empty to be default
+                React.DOM.select({className: "DropDown", multiple: true, size: 5}, itemsCode ),
             );
     }
 });
