50941a3139695bc5b9ad4b82cf20c026bcbd8057
Andrei <andreifrolov83@gmail.com>
2020-12-02 20:39:01 +0300

    update rainbow frame

--- RainbowFrame/App.js
+++ RainbowFrame/App.js
@@ -2,11 +2,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ShopBlock from './components/ShopBlock';
-import shopHeaders from './headers.json';
-import shopArr from './productList.json';
-var shopText = "Список товаров в интернет магазине";
+import MainBlock from "../RainbowFrame/components/MainBlock";
 
 ReactDOM.render(
-    <ShopBlock title={shopText} headers={shopHeaders} items={shopArr}/>, document.getElementById('container')
+    <MainBlock/>, document.getElementById('container')
 );
