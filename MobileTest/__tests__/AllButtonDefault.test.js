b7edb46ad77eea112b6d5acb2505b8868f970ae5
Andrei <andreifrolov83@gmail.com>
2020-12-12 16:23:07 +0300

    update mobile test

---
+++ MobileTest/__tests__/AllButtonDefault.test.js
@@ -0,0 +1,55 @@
+"use strict";
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import MobileCompany from '../components/MobileCompany';
+let clientsArr = [
+    {id:101, lastName:"Иванов", firstName: "Иван", otchestvo: "Иванович", balance: 200, status: true},
+    {id:102, lastName:"Сидоров", firstName: "Сидор", otchestvo: "Сидорович", balance: 250, status: true},
+    {id:103, lastName:"Петров", firstName: "Петр", otchestvo: "Петрович", balance: 180, status: true},
+    {id:104, lastName:"Григорьев", firstName: "Григорий", otchestvo: "Григорьевич", balance: -220, status: false},
+];
+
+let headers = [
+    {code:1, header: "Фамилия"},
+    {code:2, header: "Имя"},
+    {code:3, header: "Отчество"},
+    {code:4, header: "Баланс"},
+    {code:5, header: "Статус"},
+    {code:6, header: "Редактировать"},
+    {code:7, header: "Удалить"},
+];
+
+test('работа кнопки "Все" при открытии страницы', () => {
+
+    // создаём тестовую версию компонента
+    const component = renderer.create(
+        <MobileCompany
+            headers = {headers}
+            clients={clientsArr}
+        />
+    );
+
+    // получаем снэпшот (HTML-снимок) компонента для сверки, что вёрстка не испортилась
+    let componentTree=component.toJSON();
+    expect(componentTree).toMatchSnapshot('AllButtonDefault.test.js.snap');
+
+    // найдём в вёрстке компонента саму кнопку
+    const buttonElem = component.root.find( el => (el.type==='input' && el.props.value === 'Все') );
+    // и "нажмём" на неё
+    buttonElem.props.onClick();
+
+    // получаем уже изменённый снэпшот
+    componentTree=component.toJSON();
+    expect(componentTree).toMatchSnapshot('AllButtonDefault.test.js.snap');
+
+    // "нажмём" кнопку ещё раз
+    buttonElem.props.onClick();
+
+    // и получаем окончательный снэпшот
+    componentTree=component.toJSON();
+    expect(componentTree).toMatchSnapshot('AllButtonDefault.test.js.snap');
+
+
+});
