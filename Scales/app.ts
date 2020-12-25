15793f05fc0ee8c2f826a01ee245270f26d35dc8
Andrei <andreifrolov83@gmail.com>
2020-12-25 14:35:40 +0300

    update scale

--- Scales/app.ts
+++ Scales/app.ts
@@ -1,23 +1,25 @@
 class Scales {
 
-    productNameList:string[] = [];
-    productScaleList:number[] = [];
+    productList:Product[] = [];
 
-    add(productName:string, productScale:number):void {
-        this.productNameList.push(productName);
-        this.productScaleList.push(productScale);
+    add(productItem: Product):void {
+        this.productList.push(productItem);
     }
 
     getSumScale():number {
         let result:number = 0;
-        for (let item of this.productScaleList) {
-            result += item;
+        for (let item of this.productList) {
+            result += item.getScale();
         }
         return result;
     }
 
     getNameList():string[] {
-        return this.productNameList;
+        let result:string[] = [];
+        for (let item of this.productList) {
+            result.push(item.getName());
+        }
+        return result;
     }
 }
 
@@ -44,24 +46,43 @@ class Apple extends Product {
     constructor(productName:string, productScale:number) {
         super(productName, productScale);
     }
+
+    appleColor:string;
+
+    setAppleColor(_appleColor:string):void {
+        this.appleColor = _appleColor;
+    }
 }
 class Tomato extends Product {
+
+    isCherry:boolean;
+
     constructor(productName:string, productScale:number) {
         super(productName, productScale);
+        this.isCherry = false;
+    }
+
+    setTomatoType(_tomatoType:boolean):void {
+        this.isCherry = _tomatoType;
     }
 }
 class Orange extends Product {
+
     constructor(productName:string, productScale:number) {
         super(productName, productScale);
     }
 }
 let scales:Scales = new Scales();
 let apple:Apple = new Apple("apple", 3);
-let tomato:Tomato = new Tomato("tomato", 2);
+apple.setAppleColor("green");
+let tomato1:Tomato = new Tomato("tomato1", 1);
+tomato1.setTomatoType(true);
+let tomato2:Tomato = new Tomato("tomato2", 2);
 let orange:Orange = new Orange("orange", 4);
-scales.add(apple.getName(), apple.getScale());
-scales.add(tomato.getName(), tomato.getScale());
-scales.add(orange.getName(), orange.getScale());
+scales.add(apple);
+scales.add(tomato1);
+scales.add(tomato2);
+scales.add(orange);
 console.log(scales.getSumScale());
 console.log(scales.getNameList());
 
