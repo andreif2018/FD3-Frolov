45c40ae4207a9b4bc0c6a40ddbfb4bab8172f8b7
Andrei <andreifrolov83@gmail.com>
2020-12-26 20:51:24 +0300

    routing

---
+++ my-app/src/components/AboutUsPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AboutUsPage.css';
+
+class AboutUsPage extends React.PureComponent {
+
+    render() {
+        return (
+            <div>
+                <p>This app allows you to create your own playlists</p>
+                <p>You will be able to rate your favorite songs</p>
+            </div>
+        );
+    }
+}
+
+export default AboutUsPage;
