f9d59fe2092d2b07222a69f6ffe6e00e4a0dbacf
Andrei <andreifrolov83@gmail.com>
2021-01-23 18:46:22 +0300

    fix mobile view

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,8 +17,19 @@ import {myEvents} from "./components/events";
 
 class App extends React.PureComponent {
 
+    constructor(props) {
+        super(props);
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+
     state = {
         badge: 'none',
+        width: 0,
+        menuState: 'none',
+    }
+
+    updateWindowDimensions() {
+        this.setState({ width: window.innerWidth });
     }
 
     addBadge = () => {
@@ -29,21 +40,33 @@ class App extends React.PureComponent {
         this.setState({badge: "none"});
     };
 
+    openCloseBurger = () => {
+        console.log("click burger");
+        console.log(this.state.menuState);
+        if (this.state.menuState === 'none') this.setState({menuState: 'block'});
+        else if (this.state.menuState === 'block') this.setState({menuState: 'none'});
+    }
+
     componentDidMount = () => {
         myEvents.addListener('UpdateBadge', this.addBadge);
         myEvents.addListener('RemoveBadge', this.removeBadge);
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
     };
 
     componentWillUnmount = () => {
         myEvents.removeListener('UpdateBadge', this.addBadge);
         myEvents.addListener('RemoveBadge', this.removeBadge);
+        window.removeEventListener('resize', this.updateWindowDimensions);
     };
 
     render() {
+        let mobileBurgerClassName = (window.innerWidth > 500) ? "BurgerWeb" : "BurgerMobile";
+        let appHeaderClassName = (window.innerWidth > 500) ? "App-header" : "AppHeaderMobile";
         return (
             <BrowserRouter>
                 <div className="App-logo"><img src={logo} alt={"logo of the app"}/></div>
-                <header className="App-header">
+                <header className={appHeaderClassName}>
                     <NavLink to="/allMusic">All Music</NavLink>
                     <NavLink to="/myLib">My Library</NavLink>
                     <NavLink to="/playList">PlayList
@@ -51,6 +74,20 @@ class App extends React.PureComponent {
                     </NavLink>
                     <NavLink to="/about">About Us</NavLink>
                 </header>
+                <div className={mobileBurgerClassName}>
+                    <img className="Burger" onClick={this.openCloseBurger}
+                        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAARUlEQVRYR+3U0QkAIAgFQN1/6IL6aQIVOhd4cujLaJ5szg8LEHgFVvFBnmwLEBglUPwEN25UEekBAgT0gBsgQEAP/CmwAdmEEiEL49BKAAAAAElFTkSuQmCC"
+                        alt="Открытие меню" title="Открытие меню"/><br/><br/>
+                        <div style={{display: this.state.menuState}}>
+                            <NavLink to="/allMusic">All Music</NavLink><br/><br/>
+                            <NavLink to="/myLib">My Library</NavLink><br/><br/>
+                            <NavLink to="/playList">PlayList
+                                <Badge name="badge" label={"new"} style={{borderRadius: '50%', display: this.state.badge}}/>
+                            </NavLink><br/><br/>
+                            <NavLink to="/about">About Us</NavLink>
+                        </div>
+
+                </div>
                 <Switch>
                     <Route exact path="/">
                         <Redirect to="/about"/>
