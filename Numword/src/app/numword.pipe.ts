99b358c73c5ed8139f1c04a998a85b7f90200db3
Andrei <andreifrolov83@gmail.com>
2021-02-11 07:17:43 +0300

    update of numword

---
+++ Numword/src/app/numword.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name:"numword",
+  pure:true,
+})
+export class NumwordPipe implements PipeTransform {
+
+  transform(amountS:string, word1:string, word2:string, word5:string):string {
+    let amount = parseInt(amountS);
+    let dd = amount%100;
+    if ( (dd >= 11) && (dd <= 19) ) return amountS + ' ' + word5;
+    let d = amount%10;
+    if ( d == 1 ) return amountS + ' ' + word1;
+    if ( (d >=2 ) && (d <= 4) ) return amountS + ' ' + word2;
+    if (amount === 0 || isNaN(amount)) return '0 ' + word5
+    return word5;
+  }
+}
